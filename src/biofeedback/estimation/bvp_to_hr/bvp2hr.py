c1b9603db5d86389b9c9a77a1d0ffed2ab77d367
Adam Katav <adamkatav@gmail.com>
2024-04-07 09:47:20 +0300

    Testing get_hr_from_bvp

--- src/biofeedback/estimation/bvp_to_hr/bvp2hr.py
+++ src/biofeedback/estimation/bvp_to_hr/bvp2hr.py
@@ -1,32 +1,38 @@
 import numpy as np
-from scipy.fft import fft, fftfreq
+from scipy.fft import fft, fftfreq, correlate, find_peaks
 from biofeedback.estimation.base_functions import bandpass_filter
+from functools import partial
 
 SAMPLING_RATE = 64 #Hz
 LOW_FREQ = 0.5 #Hz
 HIGH_FREQ = 5 #Hz
 
-def get_hr_from_bvp(bvp: np.ndarray) -> float:
-    import matplotlib.pyplot as plt
-    # plt.plot(bvp)
+hr_history: list[float] = []
+def get_hr_from_bvp(bvp: np.ndarray, hr_history: list[float], reset=False) -> float:
+    if reset:
+        hr_history = []
     filtered_bvp_signal = bandpass_filter(bvp, LOW_FREQ, HIGH_FREQ, sampling_rate=SAMPLING_RATE)
-    # n = len(filtered_bvp_signal)
-    # frequencies = fftfreq(n, 1/SAMPLING_RATE)
-    # fft_values = abs(fft(filtered_bvp_signal))
-    # frequencies = frequencies[:len(frequencies)//2]
-    # fft_values = fft_values[:len(fft_values)//2]
-    # fft_values /= sum(fft_values)
-    # hr_in_hz = fft_values@frequencies
-    # hr_bpm = 60*hr_in_hz
-    # return hr_bpm
-    from scipy.signal import correlate, find_peaks
     r = correlate(filtered_bvp_signal, filtered_bvp_signal)
     r = r[len(r)//2:]
     peaks,_ = find_peaks(r, prominence=r[0]/4)
-    # !!!Show graphs!!!
+    def b(peaks):
+        return 60/((peaks[1]-peaks[0])/64)
+    MIN_HR, MAX_HR, FALLBACK_HR = 55, 160, 60
+    if len(peaks) < 2 or b(peaks) < MIN_HR or b(peaks) > MAX_HR:
+        if len(hr_history) == 0:
+            return FALLBACK_HR
+        else:
+            return hr_history[-1]
+    hr = b(peaks)
+    print(f'msg form get_blabla: {hr=}')
+    if len(hr_history) > 2:
+        hr_history.pop(0)
+    hr_history.append(hr)
+    w = np.array([i+1 for i in range(len(hr_history))])
+    hr_history[-1] = sum([hr_history[i]*w[i] for i in range(len(hr_history))])/sum(w)
+    
+    return hr_history[-1]
     # plt.plot(r)
     # plt.scatter(peaks, r[peaks], color='r')
     # plt.show()
-    if len(peaks) < 2:
-        return 1
-    return 60/((peaks[1]-peaks[0])/64)
+    
